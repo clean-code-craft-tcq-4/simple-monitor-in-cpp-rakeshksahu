49c7b0e7fd870d85fc353569a6731835981b1377
Sahu, Rakesh Kumar (BSH) <sahu.rakesh-ext@bshg.com>
2022-07-01 10:42:48 +0530

    added function

--- checker.cpp
+++ checker.cpp
@@ -2,14 +2,18 @@
 #include <iostream>
 using namespace std;
 
+bool findIfNotInRange(float value, float min, float max)
+{
+    return (value < min || value > max);
+}
 bool batteryIsOk(float temperature, float soc, float chargeRate) {
-  if(temperature < 0 || temperature > 45) {
+  if(findIfNotInRange(temperature, 0, 45)) {
     cout << "Temperature out of range!\n";
     return false;
-  } else if(soc < 20 || soc > 80) {
+  } else if(findIfNotInRange(soc, 20, 80)) {
     cout << "State of Charge out of range!\n";
     return false;
-  } else if(chargeRate > 0.8) {
+  } else if(findIfNotInRange(chargeRate ,0, 0.8)) {
     cout << "Charge Rate out of range!\n";
     return false;
   }
