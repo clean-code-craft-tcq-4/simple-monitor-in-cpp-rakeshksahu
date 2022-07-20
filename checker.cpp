570f7a1eb7061987e5e59ca992484dac92b4ff93
Sahu, Rakesh Kumar (BSH) <sahu.rakesh-ext@bshg.com>
2022-07-20 20:43:21 +0530

    worked on review comments

--- checker.cpp
+++ checker.cpp
@@ -1,25 +1,22 @@
 #include <assert.h>
 #include <iostream>
-#include <memory>
 #include "checker.hpp"
 #include "BMSParam.hpp"
-#include "MessageProvider.hpp"
+
 using namespace std;
 
-bool batteryIsApproachingThreshold(float temperature, float soc, float chargeRate)
-{
-  std::shared_ptr<MessageProvider> engMsgProvider(new EnglishMessageProvider()); 
-  std::shared_ptr<BMSParam> temp(new Temperature(temperature, engMsgProvider.get())); 
-  std::shared_ptr<BMSParam> stateOfCharge(new SOC(soc, engMsgProvider.get()));
-  std::shared_ptr<BMSParam> rateOfCharge(new ChargeRate(chargeRate, engMsgProvider.get()));
+bool Checker::batteryIsApproachingThreshold(float temperature, float soc, float chargeRate)
+{ 
+  std::shared_ptr<BMSParam> temp(new Temperature(temperature, msgProvider.get())); 
+  std::shared_ptr<BMSParam> stateOfCharge(new SOC(soc, msgProvider.get()));
+  std::shared_ptr<BMSParam> rateOfCharge(new ChargeRate(chargeRate, msgProvider.get()));
   return (temp->checkIfToleranceApproaching() || stateOfCharge->checkIfToleranceApproaching() || rateOfCharge->checkIfToleranceApproaching());
 }
 
-bool batteryIsOk(float temperature, float soc, float chargeRate)
+bool Checker::batteryIsOk(float temperature, float soc, float chargeRate)
 {
-  std::shared_ptr<MessageProvider> grMsgProvider(new GermanMessageProvider()); 
-  std::shared_ptr<BMSParam> temp(new Temperature(temperature, grMsgProvider.get())); 
-  std::shared_ptr<BMSParam> stateOfCharge(new SOC(soc, grMsgProvider.get()));
-  std::shared_ptr<BMSParam> rateOfCharge(new ChargeRate(chargeRate, grMsgProvider.get()));
+  std::shared_ptr<BMSParam> temp(new Temperature(temperature, msgProvider.get())); 
+  std::shared_ptr<BMSParam> stateOfCharge(new SOC(soc, msgProvider.get()));
+  std::shared_ptr<BMSParam> rateOfCharge(new ChargeRate(chargeRate, msgProvider.get()));
   return (temp->checkWithinRange() && stateOfCharge->checkWithinRange() && rateOfCharge->checkWithinRange());
 }
--- test_checker.cpp
+++ test_checker.cpp
@@ -3,26 +3,29 @@
 using namespace std;
 
 int main() {
-  assert(batteryIsOk(0, 70, 0.7) == false);
-  assert(batteryIsOk(25, 70, 0.7) == true);
-  assert(batteryIsOk(50, 70, 0.7) == false);
-  assert(batteryIsOk(25, 10, 0.7) == false);
-  assert(batteryIsOk(25, 85, 0.5) == false);
-  assert(batteryIsOk(30, 70, 0.0) == false);
-  assert(batteryIsOk(30, 70, 0.1) == true);
+  Checker checker;
+  //checker.msgProvider = std::make_shared<EnglishMessageProvider>();
+  checker.msgProvider = std::make_shared<GermanMessageProvider>();
+  assert(checker.batteryIsOk(0, 70, 0.7) == false);
+  assert(checker.batteryIsOk(25, 70, 0.7) == true);
+  assert(checker.batteryIsOk(50, 70, 0.7) == false);
+  assert(checker.batteryIsOk(25, 10, 0.7) == false);
+  assert(checker.batteryIsOk(25, 85, 0.5) == false);
+  assert(checker.batteryIsOk(30, 70, 0.0) == false);
+  assert(checker.batteryIsOk(30, 70, 0.1) == true);
 
-  assert(batteryIsApproachingThreshold(1, 70, 0.7) == true);
-  assert(batteryIsApproachingThreshold(44, 70, 0.7) == true);
-  assert(batteryIsApproachingThreshold(40, 70, 0.7) == false);
-  assert(batteryIsApproachingThreshold(50, 70, 0.7) == false);
-  assert(batteryIsApproachingThreshold(25, 22, 0.7) == true);
-  assert(batteryIsApproachingThreshold(25, 78, 0.7) == true);
-  assert(batteryIsApproachingThreshold(25, 13, 0.7) == false);
-  assert(batteryIsApproachingThreshold(25, 85, 0.7) == false);
-  assert(batteryIsApproachingThreshold(25, 40, 0.7) == false);
-  assert(batteryIsApproachingThreshold(25, 70, 0.7) == false);
-  assert(batteryIsApproachingThreshold(25, 70, 0.79) == true);
-  assert(batteryIsApproachingThreshold(25, 70, 0.01) == true);
-  assert(batteryIsApproachingThreshold(25, 70, 0.0) == false);
-  assert(batteryIsApproachingThreshold(25, 70, 0.9) == false);
+  assert(checker.batteryIsApproachingThreshold(1, 70, 0.7) == true);
+  assert(checker.batteryIsApproachingThreshold(44, 70, 0.7) == true);
+  assert(checker.batteryIsApproachingThreshold(40, 70, 0.7) == false);
+  assert(checker.batteryIsApproachingThreshold(50, 70, 0.7) == false);
+  assert(checker.batteryIsApproachingThreshold(25, 22, 0.7) == true);
+  assert(checker.batteryIsApproachingThreshold(25, 78, 0.7) == true);
+  assert(checker.batteryIsApproachingThreshold(25, 13, 0.7) == false);
+  assert(checker.batteryIsApproachingThreshold(25, 85, 0.7) == false);
+  assert(checker.batteryIsApproachingThreshold(25, 40, 0.7) == false);
+  assert(checker.batteryIsApproachingThreshold(25, 70, 0.7) == false);
+  assert(checker.batteryIsApproachingThreshold(25, 70, 0.79) == true);
+  assert(checker.batteryIsApproachingThreshold(25, 70, 0.01) == true);
+  assert(checker.batteryIsApproachingThreshold(25, 70, 0.0) == false);
+  assert(checker.batteryIsApproachingThreshold(25, 70, 0.9) == false);
 }
